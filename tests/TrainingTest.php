4d08abd9c47f2dc9b6f3393b8bd783d3ada9fc89
Johann <johann.delebarre@letsco.co>
2023-06-27 11:23:02 +0200

    Test

---
+++ tests/TrainingTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Letsco\Training\Test;
+
+use SilverStripe\Dev\SapphireTest;
+
+class TrainingTest extends SapphireTest
+{
+    public function test()
+    {
+        echo "TODO";
+    }
+}
